9f00b74de888921d393c8a8d50f144dad028cc77
Isaac Steiner <ilebwohlsteiner@gmail.com>
2024-10-20 16:03:31 -0700

    update

--- nametag.py
+++ nametag.py
@@ -2,7 +2,6 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
-import jsonpickle
 from datetime import datetime
 import logging
 from waveshare_epd import epd5in65f
@@ -87,8 +86,7 @@ def get_weather_icon_path(weather_code, is_night=False):
 
 # Determine if it's night based on the current time and sunset time
 current_time = datetime.now()
-logging.info(jsonpickle.encode(response))
-logging.info(str(daily_sunset))
+logging.info("Sunset: " & str(daily_sunset))
 sunset_time = datetime.fromisoformat(daily_sunset[0])
 
 is_night = current_time > sunset_time
