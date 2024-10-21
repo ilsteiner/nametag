616763b4153a0bf12ff212ecb929ef4fd706bd27
Isaac Steiner <ilebwohlsteiner@gmail.com>
2024-10-20 21:26:32 -0700

    sdffsf

--- nametag.py
+++ nametag.py
@@ -61,7 +61,7 @@ daily_data["precip"] = daily_precipitation_probability
 
 weather_dataframe = pd.DataFrame(data = daily_data)
 
-logging.info("Sunrise:" + str(daily_sunrise.Values(0)))
+logging.info("Sunrise:" + daily_sunrise.Variable)
 
 # Extract today's and tomorrow's data using iloc
 today_max = weather_dataframe.iloc[0]["temp_high"]
